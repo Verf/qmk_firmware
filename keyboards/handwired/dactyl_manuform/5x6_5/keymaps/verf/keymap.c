4494e407ff973acd3d211b987b897440cf6ad2a7
Verf <verf@proton.me>
2026-06-08 18:31:00 +0800

    map to cantor

---
+++ keyboards/handwired/dactyl_manuform/5x6_5/keymaps/verf/keymap.c
@@ -0,0 +1,153 @@
+// Copyright 2022 Diego Palacios (@diepala)
+// SPDX-License-Identifier: GPL-2.0
+// Ported from Cantor layout to Dactyl Manuform 5x6_5
+
+#include QMK_KEYBOARD_H
+#if __has_include("keymap.h")
+#    include "keymap.h"
+#endif
+
+const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM =
+    LAYOUT_5x6_5(
+        'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R',
+        'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R',
+        'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R',
+        'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R',
+             '*', '*', '*', '*', '*',  '*', '*', '*', '*', '*',
+                            '*', '*',  '*', '*'
+    );
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+     * base
+     *
+     * ┌───┬───┬───┬───┬───┬───┐   ┌───┬───┬───┬───┬───┬───┐
+     * │ESC│ 1 │ 2 │ 3 │ 4 │ 5 │   │ 6 │ 7 │ 8 │ 9 │ 0 │DEL│
+     * ├───┼───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┼───┤
+     * │TAB│ Q │ W │ E │ R │ T │   │ Y │ U │ I │ O │ P │ \ │
+     * ├───┼───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┼───┤
+     * │CwT│ A │ S⌥│ D⇧│ F⌃│ G │   │ H │ J⌃│ K⇧│ L⌥│ ; │ ' │
+     * ├───┼───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┼───┤
+     * │ ⇧ │ Z │ X │ C │ V │ B │   │ N │ M │ , │ . │ / │ ` │
+     * ├───┼───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┼───┤
+     * │   │   │   │   │Alt│Tab│   │ ⇥ │Ent│Bsp│   │   │   │
+     * ├───┼───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┼───┤
+     *                     │Sp1│   │Gui│
+     *                     ├───┤   ├───┤
+     *                     │   │   │ ⇥ │
+     *                     └───┘   └───┘
+     */
+    [0] = LAYOUT_5x6_5(
+        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+        LCTL_T(CW_TOGG), KC_A, LALT_T(KC_S), LSFT_T(KC_D), LCTL_T(KC_F), KC_G,   KC_H,   RCTL_T(KC_J),    RSFT_T(KC_K), LALT_T(KC_L), KC_SCLN, KC_QUOT,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_GRV,
+                                   KC_NO,   KC_NO,   KC_NO,   KC_NO,   LT(1,KC_SPC),    KC_LGUI,  KC_NO,   KC_NO,   KC_NO,   KC_NO,
+                                   KC_TAB,  KC_LALT, LT(2,KC_ENT),   KC_BSPC
+    ),
+    /*
+     * symbol
+     *
+     * ┌───┬───┬───┬───┬───┬───┐   ┌───┬───┬───┬───┬───┬───┐
+     * │   │   │   │   │   │   │   │   │   │   │   │   │   │
+     * ├───┼───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┼───┤
+     * │   │LA1│LA2│LA3│LA4│   │   │ - │ + │ [ │ ] │ \ │   │
+     * ├───┼───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┼───┤
+     * │   │Hom│PDn│PUp│End│ ! │   │ _ │ = │ ( │ ) │ | │   │
+     * ├───┼───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┼───┤
+     * │   │ ← │ ↓ │ ↑ │ → │ % │   │ ^ │ $ │ { │ } │ & │   │
+     * ├───┼───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┼───┤
+     * │   │   │   │   │TRS│BOT│   │ * │ @ │ # │   │   │   │
+     * ├───┼───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┼───┤
+     *                     │TRS│   │Ast│
+     *                     ├───┤   ├───┤
+     *                     │   │   │ @ │
+     *                     └───┘   └───┘
+     */
+    [1] = LAYOUT_5x6_5(
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_NO, LALT(KC_1), LALT(KC_2), LALT(KC_3), LALT(KC_4), KC_NO,    KC_MINS, KC_PLUS, KC_LBRC, KC_RBRC, KC_BSLS, KC_NO,
+        KC_NO, KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_EXLM,               KC_UNDS, KC_EQL,  KC_LPRN, KC_RPRN, KC_PIPE, KC_NO,
+        KC_NO, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_PERC,                KC_CIRC, KC_DLR,  KC_LCBR, KC_RCBR, KC_AMPR, KC_NO,
+                                 KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS,    KC_ASTR,  KC_NO,   KC_NO,   KC_NO,   KC_NO,
+                                 QK_BOOT, KC_TRNS, KC_AT,   KC_HASH
+    ),
+    /*
+     * 0-9 / F1-F12 / func
+     *
+     * ┌───┬───┬───┬───┬───┬───┐   ┌───┬───┬───┬───┬───┬───┐
+     * │F1 │F2 │F3 │F4 │F5 │F6 │   │F7 │F8 │F9 │F10│F11│F12│
+     * ├───┼───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┼───┤
+     * │   │ 1 │ 2 │ 3 │ 4 │ 5 │   │ 6 │ 7 │ 8 │ 9 │ 0 │   │
+     * ├───┼───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┼───┤
+     * │   │   │   │   │   │   │   │   │   │   │   │   │   │
+     * ├───┼───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┼───┤
+     * │   │   │   │   │   │   │   │   │   │   │   │   │   │
+     * ├───┼───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┼───┤
+     * │   │   │   │   │   │   │   │TRS│TRS│BOT│   │   │   │
+     * ├───┼───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┼───┤
+     *                     │   │   │TRS│
+     *                     ├───┤   ├───┤
+     *                     │   │   │BOT│
+     *                     └───┘   └───┘
+     */
+    [2] = LAYOUT_5x6_5(
+        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,        KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+        KC_NO,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_NO,
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+                                  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_TRNS,  KC_NO,   KC_NO,   KC_NO,   KC_NO,
+                                  KC_NO,   KC_NO,   KC_TRNS,   QK_BOOT
+    )
+};
+
+bool caps_word_press_user(uint16_t keycode) {
+    switch (keycode) {
+        // Keycodes that continue Caps Word, with shift applied.
+        case KC_A ... KC_Z:
+        case KC_SCLN:
+            add_weak_mods(MOD_BIT(KC_LSFT));  // Apply shift to next key.
+            return true;
+
+        // Keycodes that continue Caps Word, without shifting.
+        case KC_1 ... KC_0:
+        case KC_BSPC:
+        case KC_MINS:
+        case KC_UNDS:
+            return true;
+
+        default:
+            return false;  // Deactivate Caps Word.
+    }
+}
+
+bool is_flow_tap_key(uint16_t keycode) {
+    if ((get_mods() & (MOD_MASK_CG | MOD_BIT_LALT)) != 0) {
+        return false; // Disable Flow Tap on hotkeys.
+    }
+    switch (get_tap_keycode(keycode)) {
+        case KC_A ... KC_Z:
+        case KC_DOT:
+        case KC_COMM:
+        case KC_SCLN:
+        case KC_SLSH:
+            return true;
+    }
+    return false;
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case LCTL_T(CW_TOGG):
+            if (record->tap.count && record->event.pressed) {
+                caps_word_on();
+                return false;        // Return false to ignore further processing of key
+            }
+            break;
+    }
+    return true;
+}
+
+#ifdef OTHER_KEYMAP_C
+#    include OTHER_KEYMAP_C
+#endif // OTHER_KEYMAP_C
---
+++ keyboards/handwired/dactyl_manuform/5x6_5/keymaps/verf/rules.mk
@@ -0,0 +1,3 @@
+CAPS_WORD_ENABLE = yes
+CHORDAL_HOLD = yes
+FLOW_TAP_TERM = yes
