edc63126daaf4b85de5e41a2a3b5d4cea876e184
Verf <verf@proton.me>
2026-06-08 09:17:40 +0800

    add func to switch virtual desktop and func

--- keyboards/cantor/keymaps/verf/keymap.c
+++ keyboards/cantor/keymaps/verf/keymap.c
@@ -31,26 +31,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ├───┼───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┼───┤
      * │   │End│ ← │ ↓ │ → │ ! │   │ _ │ = │ ( │ ) │ | │   │
      * ├───┼───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┼───┤
-     * │   │   │   │   │ % │ * │   │ ^ │ $ │ { │ } │ & │   │
+     * │   │   │CGL│CGR│ % │ * │   │ ^ │ $ │ { │ } │ & │   │
      * ├───┼───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┼───┤
      * │   │   │   │   │BOT│TRS│   │   │ @ │   │   │   │   │
      * └───┴───┴───┴───┴───┴───┘   └───┴───┴───┴───┴───┴───┘
      */
     [1] = LAYOUT_split_3x6_3(
-        KC_NO,  KC_HOME, KC_PGUP, KC_UP,   KC_PGDN, KC_HASH,          KC_MINS, KC_PLUS, KC_LBRC, KC_RBRC, KC_BSLS, KC_NO,
-        KC_NO,  KC_END,  KC_LEFT, KC_DOWN, KC_RGHT, KC_EXLM,          KC_UNDS, KC_EQL,  KC_LPRN, KC_RPRN, KC_PIPE, KC_NO,
-        KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_PERC, KC_ASTR,          KC_CIRC, KC_DLR,  KC_LCBR, KC_RCBR, KC_AMPR, KC_NO,
+        KC_NO,  KC_HOME, KC_PGUP,      KC_UP,         KC_PGDN, KC_HASH,          KC_MINS, KC_PLUS, KC_LBRC, KC_RBRC, KC_BSLS, KC_NO,
+        KC_NO,  KC_END,  KC_LEFT,      KC_DOWN,       KC_RGHT, KC_EXLM,          KC_UNDS, KC_EQL,  KC_LPRN, KC_RPRN, KC_PIPE, KC_NO,
+        KC_NO,  KC_NO,   LCG(KC_LEFT), LCG(KC_RIGHT), KC_PERC, KC_ASTR,          KC_CIRC, KC_DLR,  KC_LCBR, KC_RCBR, KC_AMPR, KC_NO,
                                   KC_NO,   QK_BOOT, KC_TRNS,          KC_NO,   KC_AT,   KC_NO
     ),
     /*
-     * 0-9 / F1-F12
+     * 0-9 / F1-F12 / func
      *
      * ┌───┬───┬───┬───┬───┬───┐   ┌───┬───┬───┬───┬───┬───┐
      * │   │ 1 │ 2 │ 3 │ 4 │ 5 │   │ 6 │ 7 │ 8 │ 9 │ 0 │   │
      * ├───┼───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┼───┤
      * │F1 │F2 │F3 │F4 │F5 │F6 │   │F7 │F8 │F9 │F10│F11│F12│
      * ├───┼───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┼───┤
-     * │   │   │   │   │   │   │   │   │   │   │   │   │   │
+     * │   │UDO│CUT│CPY│PST│FND│   │   │   │   │   │   │   │
      * ├───┼───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┼───┤
      * │   │   │   │   │   │   │   │TRS│BOT│   │   │   │   │
      * └───┴───┴───┴───┴───┴───┘   └───┴───┴───┴───┴───┴───┘
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [2] = LAYOUT_split_3x6_3(
         KC_NO,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,          KC_6,    KC_7,     KC_8,     KC_9,     KC_0,    KC_NO,
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,         KC_F7,   KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,
-        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,         KC_NO,   KC_NO,    KC_NO,    KC_NO,    KC_NO,   KC_NO,
+        KC_NO,   KC_UNDO, KC_CUT,  KC_COPY, KC_PSTE, KC_FIND,       KC_NO,   KC_NO,    KC_NO,    KC_NO,    KC_NO,   KC_NO,
                                    KC_NO,   KC_NO,   KC_NO,         KC_TRNS, QK_BOOT, KC_NO
     )
 };
