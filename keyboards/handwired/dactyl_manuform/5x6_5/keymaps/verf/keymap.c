cbd59c1394e03fc3e0a04bf66bab73d513092a2d
Verf <verf@proton.me>
2026-06-09 10:16:39 +0800

    adjust underscore and eql

--- keyboards/handwired/dactyl_manuform/5x6_5/keymaps/verf/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6_5/keymaps/verf/keymap.c
@@ -22,22 +22,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * BASE
      */
     [0] = LAYOUT_5x6_5(
-        KC_ESC,          KC_1,         KC_2,         KC_3,         KC_4,         KC_5,         /* */ KC_6,         KC_7,         KC_8,         KC_9,         KC_0,    KC_DEL,
-        KC_NO,           KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,         /* */ KC_Y,         KC_U,         KC_I,         KC_O,         KC_P,    KC_BSLS,
-        LCTL_T(CW_TOGG), LGUI_T(KC_A), LALT_T(KC_S), LSFT_T(KC_D), LCTL_T(KC_F), KC_G,         /* */ KC_H,         RCTL_T(KC_J), RSFT_T(KC_K), LALT_T(KC_L), RGUI_T(KC_SCLN), KC_QUOT,
-        KC_LSFT,         KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,         /* */ KC_N,         KC_M,         KC_COMM,      KC_DOT,       KC_SLSH, KC_GRV,
-                         KC_NO,        KC_NO,        KC_SPC,       KC_NO,        LT(3,KC_F20), /* */ LT(4,KC_EQL), KC_NO,        KC_BSPC,      KC_NO,        KC_NO,
-                                                                   LT(1,KC_TAB), KC_NO,        /* */ KC_NO,        LT(2,KC_ENT)
+        KC_ESC,          KC_1,         KC_2,         KC_3,         KC_4,         KC_5,  /* */ KC_6,  KC_7,         KC_8,         KC_9,         KC_0,            KC_DEL,
+        KC_NO,           KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,  /* */ KC_Y,  KC_U,         KC_I,         KC_O,         KC_P,            KC_BSLS,
+        LCTL_T(CW_TOGG), LGUI_T(KC_A), LALT_T(KC_S), LSFT_T(KC_D), LCTL_T(KC_F), KC_G,  /* */ KC_H,  RCTL_T(KC_J), RSFT_T(KC_K), LALT_T(KC_L), RGUI_T(KC_SCLN), KC_QUOT,
+        KC_LSFT,         KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,  /* */ KC_N,  KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,         KC_GRV,
+                         KC_NO,        KC_NO,        KC_SPC,       KC_NO,        MO(3), /* */ MO(4), KC_NO,        KC_BSPC,      KC_NO,        KC_NO,
+                                                                   LT(1,KC_TAB), KC_NO, /* */ KC_NO, LT(2,KC_ENT)
     ),
     /*
      * SYM
      */
     [1] = LAYOUT_5x6_5(
-        KC_TRNS, KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS, /* */ KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, LALT(KC_1), LALT(KC_2), LALT(KC_3), LALT(KC_4), KC_NO,   /* */ KC_PLUS, KC_NO,      KC_LBRC, KC_RBRC, KC_NO,   KC_BSLS,
-        KC_TRNS, KC_EXLM,    KC_AT,      KC_CIRC,    KC_DLR,     KC_PERC, /* */ KC_MINS, LSFT(KC_P), KC_LPRN, KC_RPRN, KC_DQUO, KC_PIPE,
-        KC_TRNS, KC_NO,      KC_NO,      KC_AMPR,    KC_HASH,    KC_ASTR, /* */ KC_LT,   KC_GT,      KC_LCBR, KC_RCBR, KC_QUES, KC_NO,
-                 KC_NO,      KC_NO,      KC_TRNS,    KC_TRNS,    QK_BOOT, /* */ KC_TRNS, KC_TRNS,    KC_NO,   KC_NO,   KC_NO,
+        KC_TRNS, KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS, /* */ KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, LALT(KC_1), LALT(KC_2), LALT(KC_3), LALT(KC_4), KC_NO,   /* */ KC_PLUS, KC_UNDS, KC_LBRC, KC_RBRC, KC_NO,   KC_BSLS,
+        KC_TRNS, KC_EXLM,    KC_AT,      KC_CIRC,    KC_DLR,     KC_PERC, /* */ KC_MINS, KC_EQL,  KC_LPRN, KC_RPRN, KC_DQUO, KC_PIPE,
+        KC_TRNS, KC_NO,      KC_NO,      KC_AMPR,    KC_HASH,    KC_ASTR, /* */ KC_LT,   KC_GT,   KC_LCBR, KC_RCBR, KC_QUES, KC_NO,
+                 KC_NO,      KC_NO,      KC_TRNS,    KC_TRNS,    QK_BOOT, /* */ KC_TRNS, KC_TRNS, KC_NO,   KC_NO,   KC_NO,
                                                      KC_TRNS,    KC_TRNS, /* */ KC_TRNS, KC_TRNS
     ),
     /*
