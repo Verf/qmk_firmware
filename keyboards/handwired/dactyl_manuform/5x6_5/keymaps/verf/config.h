18f0c6df0ef70eaf4edd7814df9136f150b41217
Verf <verf@proton.me>
2026-06-10 17:18:39 +0800

    switch ctrl and alt, adjust sym

--- keyboards/handwired/dactyl_manuform/5x6_5/keymaps/verf/config.h
+++ keyboards/handwired/dactyl_manuform/5x6_5/keymaps/verf/config.h
@@ -23,11 +23,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // === Home Row Mod Optimization ===
 #define PERMISSIVE_HOLD
-#define CHORDAL_HOLD
+//#define CHORDAL_HOLD
 
 #define TAPPING_TERM 250
 #define FLOW_TAP_TERM 150
 
 // === Caps Word ===
 #define CAPS_WORD_IDLE_TIMEOUT 0
-#define CAPS_WORD_INVERT_ON_SHIFT
--- keyboards/handwired/dactyl_manuform/5x6_5/keymaps/verf/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6_5/keymaps/verf/keymap.c
@@ -7,24 +7,24 @@
 #    include "keymap.h"
 #endif
 
-const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM =
-    LAYOUT_5x6_5(
-        'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R',
-        'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R',
-        'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R',
-        'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R',
-             '*', '*', '*', '*', '*',  '*', '*', '*', '*', '*',
-                            '*', '*',  '*', '*'
-    );
+//const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM =
+//    LAYOUT_5x6_5(
+//        'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R',
+//        'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R',
+//        'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R',
+//        'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R',
+//             '*', '*', '*', '*', '*',  '*', '*', '*', '*', '*',
+//                            '*', '*',  '*', '*'
+//    );
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
      * BASE
      */
     [0] = LAYOUT_5x6_5(
-        KC_ESC,  KC_1,         KC_2,         KC_3,         KC_4,         KC_5,  /* */ KC_6,  KC_7,         KC_8,       KC_9,         KC_0,            KC_DEL,
-        KC_NO,   KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,  /* */ KC_Y,  KC_U,         KC_I,       KC_O,         KC_P,            KC_BSLS,
-        KC_TAB,  LGUI_T(KC_A), LALT_T(KC_S), LSFT_T(KC_D), LCTL_T(KC_F), KC_G,  /* */ KC_H,  RCTL_T(KC_J), RSFT_T(KC_K), LALT_T(KC_L), RGUI_T(KC_SCLN), KC_QUOT,
+        KC_ESC,  KC_1,         KC_2,         KC_3,         KC_4,         KC_5,  /* */ KC_6,  KC_7,         KC_8,         KC_9,         KC_0,            KC_DEL,
+        KC_TAB,  KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,  /* */ KC_Y,  KC_U,         KC_I,         KC_O,         KC_P,            KC_BSLS,
+        CW_TOGG, LGUI_T(KC_A), LCTL_T(KC_S), LSFT_T(KC_D), LALT_T(KC_F), KC_G,  /* */ KC_H,  LALT_T(KC_J), RSFT_T(KC_K), RCTL_T(KC_L), RGUI_T(KC_SCLN), KC_QUOT,
         KC_LSFT, KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,  /* */ KC_N,  KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,         KC_GRV,
                  KC_NO,        KC_NO,        KC_SPC,       KC_NO,        MO(3), /* */ MO(4), KC_NO,        KC_BSPC,      KC_NO,        KC_NO,
                                                            MO(1),        KC_NO, /* */ KC_NO, LT(2,KC_ENT)
@@ -35,9 +35,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [1] = LAYOUT_5x6_5(
         KC_TRNS, KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS, /* */ KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, LALT(KC_1), LALT(KC_2), LALT(KC_3), LALT(KC_4), KC_NO,   /* */ KC_PLUS, KC_UNDS, KC_LBRC, KC_RBRC, KC_BSLS, KC_TRNS,
-        CW_TOGG, KC_EXLM,    KC_AT,      KC_CIRC,    KC_DLR,     KC_PERC, /* */ KC_MINS, KC_EQL,  KC_LPRN, KC_RPRN, KC_PIPE, KC_DQUO,
+        KC_TRNS, KC_EXLM,    KC_AT,      KC_CIRC,    KC_DLR,     KC_PERC, /* */ KC_MINS, KC_EQL,  KC_LPRN, KC_RPRN, KC_PIPE, KC_DQUO,
         KC_TRNS, KC_NO,      KC_NO,      KC_AMPR,    KC_HASH,    KC_ASTR, /* */ KC_LT,   KC_GT,   KC_LCBR, KC_RCBR, KC_QUES, KC_NO,
-                 KC_NO,      KC_NO,      KC_TRNS,    KC_TRNS,    QK_BOOT, /* */ KC_TRNS, KC_TRNS, KC_NO,   KC_NO,   KC_NO,
+                 KC_NO,      KC_NO,      KC_TRNS,    KC_TRNS,    QK_BOOT, /* */ KC_TRNS, KC_TRNS, KC_NO,   KC_NO,   KC_TILD,
                                                      KC_TRNS,    KC_TRNS, /* */ KC_TRNS, KC_TRNS
     ),
     /*
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,   /* */ KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO,  KC_NO,
         KC_F1, KC_F2, KC_F3, KC_F4,   KC_F5,   KC_F6,   /* */ KC_F7,   KC_F8,   KC_F9,   KC_F10, KC_F11, KC_F12,
         KC_NO, KC_1,  KC_2,  KC_3,    KC_4,    KC_5,    /* */ KC_6,    KC_7,    KC_8,    KC_9,   KC_0,   KC_NO,
-        KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,   /* */ KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO,  KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,   /* */ KC_NO,   KC_NO,   KC_TRNS, KC_TRNS,KC_NO,  KC_NO,
                KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, /* */ QK_BOOT, KC_TRNS, KC_TRNS, KC_NO,  KC_NO,
                                       KC_TRNS, KC_TRNS, /* */ KC_TRNS, KC_TRNS
     ),
@@ -111,3 +111,8 @@ bool is_flow_tap_key(uint16_t keycode) {
     }
     return false;
 }
+
+const uint16_t PROGMEM qw_combo[] = {KC_Q, KC_W, COMBO_END};
+combo_t key_combos[] = {
+    COMBO(qw_combo, KC_ESC),
+};
