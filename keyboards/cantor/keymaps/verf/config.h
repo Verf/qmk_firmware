87b40a96ae2aee59407d266506eb238d75b99a22
Verf <verf@proton.me>
2026-06-12 16:37:51 +0800

    adjust

--- keyboards/cantor/keymaps/verf/config.h
+++ keyboards/cantor/keymaps/verf/config.h
@@ -3,10 +3,10 @@
 
 #pragma once
 
-#define PERMISSIVE_HOLD
 #define CHORDAL_HOLD
 
 #define TAPPING_TERM 200
-#define FLOW_TAP_TERM 150
+#define FLOW_TAP_TERM 200
+#define COMBO_TERM 75
 
 #define CAPS_WORD_IDLE_TIMEOUT 0
--- keyboards/cantor/keymaps/verf/keymap.c
+++ keyboards/cantor/keymaps/verf/keymap.c
@@ -9,10 +9,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * BASE
      */
     [0] = LAYOUT_split_3x6_3(
-        KC_ESC,  KC_Q, KC_W,         KC_E,         KC_R,         KC_T,  /* */ KC_Y,    KC_U,         KC_I,           KC_O,          KC_P,    KC_BSLS,
+        KC_TAB,  KC_Q, KC_W,         KC_E,         KC_R,         KC_T,  /* */ KC_Y,    KC_U,         KC_I,           KC_O,          KC_P,    KC_BSLS,
         CW_TOGG, KC_A, LCTL_T(KC_S), LSFT_T(KC_D), LALT_T(KC_F), KC_G,  /* */ KC_H,    LALT_T(KC_J), RSFT_T(KC_K),   RCTL_T(KC_L),  KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z, LCS_T(KC_X),  LCA_T(KC_C),  LSA_T(KC_V),  KC_B,  /* */ KC_N,    LSA_T(KC_M),  RCA_T(KC_COMM), RCS_T(KC_DOT), KC_SLSH, KC_GRV,
-                                     KC_TAB,       LT(1,KC_SPC), MO(3), /* */ KC_LGUI, LT(2,KC_ENT), KC_BSPC
+                                     KC_ESC,       LT(1,KC_SPC), MO(3), /* */ KC_LGUI, LT(2,KC_ENT), KC_BSPC
     ),
     /*
      * SYM
@@ -21,25 +21,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO,         LALT(KC_1), LALT(KC_2), LALT(KC_3), LALT(KC_4), KC_NO,   /* */ KC_PLUS, KC_MINS, KC_LBRC, KC_RBRC, KC_NO,   KC_PIPE,
         KC_NO,         KC_EXLM,    KC_CIRC,    KC_AT,      KC_DLR,     KC_PERC, /* */ KC_UNDS, KC_EQL,  KC_LPRN, KC_RPRN, KC_NO,   KC_DQUO,
         LCTL(KC_BSLS), KC_NO,      KC_NO,      KC_AMPR,    KC_HASH,    KC_ASTR, /* */ KC_LT,   KC_GT,   KC_LCBR, KC_RCBR, KC_QUES, KC_TILD,
-                                               QK_BOOT,    KC_TRNS,    QK_BOOT, /* */ KC_NO,   KC_NO,   KC_NO
+                                               QK_BOOT,    KC_TRNS,    KC_TRNS, /* */ DT_UP,   DT_DOWN, DT_PRNT
     ),
     /*
      * NUM
      */
     [2] = LAYOUT_split_3x6_3(
-        KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, /* */ KC_F7,   KC_F8,   KC_F9,  KC_F10, KC_F11, KC_F12,
-        KC_NO, KC_1,  KC_2,  KC_3,  KC_4,  KC_5,  /* */ KC_6,    KC_7,    KC_8,   KC_9,   KC_0,   KC_NO,
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, /* */ KC_NO,   KC_NO,   KC_NO,  KC_NO,  KC_NO,  KC_NO,
-                             KC_NO, KC_NO, KC_NO, /* */ QK_BOOT, KC_TRNS, QK_BOOT
+        KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, /* */ KC_F7, KC_F8,   KC_F9, KC_F10, KC_F11, KC_F12,
+        KC_NO, KC_1,  KC_2,  KC_3,  KC_4,  KC_5,  /* */ KC_6,  KC_7,    KC_8,  KC_9,   KC_0,   KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, /* */ KC_NO, KC_NO,   KC_NO, KC_NO,  KC_NO,  KC_NO,
+                             KC_NO, KC_NO, KC_NO, /* */ KC_NO, KC_TRNS, QK_BOOT
     ),
     /*
      * NAV
      */
     [3] = LAYOUT_split_3x6_3(
-        KC_NO, KC_NO,      KC_PGUP,    KC_UP,      KC_PGDN,    KC_HOME, /* */ KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_NO, LCA(KC_DEL),
-        KC_NO, LCTL(KC_A), KC_LEFT,    KC_DOWN,    KC_RGHT,    KC_END,  /* */ KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_NO, KC_NO,
-        KC_NO, LCTL(KC_Z), LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), KC_NO,   /* */ KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,
-                                       KC_NO,      KC_NO,      KC_TRNS, /* */ KC_NO,   KC_NO,   KC_DEL
+        KC_NO,         KC_NO,      KC_PGUP,    KC_UP,      KC_PGDN,    KC_HOME, /* */ KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_NO, LCA(KC_DEL),
+        KC_NO,         LCTL(KC_A), KC_LEFT,    KC_DOWN,    KC_RGHT,    KC_END,  /* */ KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_NO, KC_NO,
+        LCTL(KC_BSLS), LCTL(KC_Z), LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), KC_NO,   /* */ KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,
+                                               QK_BOOT,    KC_TRNS,    KC_TRNS, /* */ KC_NO,   KC_NO,   KC_DEL
     ),
 };
 
@@ -72,9 +72,25 @@ bool caps_word_press_user(uint16_t keycode) {
     }
 }
 
+bool is_flow_tap_key(uint16_t keycode) {
+    if ((get_mods() & (MOD_MASK_CG | MOD_BIT_LALT)) != 0) {
+        return false; // Disable Flow Tap on hotkeys.
+    }
+    switch (get_tap_keycode(keycode)) {
+        // case KC_SPC:
+        case KC_A ... KC_Z:
+        case KC_DOT:
+        case KC_COMM:
+        // case KC_SCLN:
+        case KC_SLSH:
+            return true;
+    }
+    return false;
+}
+
 const uint16_t PROGMEM qw_combo[] = {KC_Q, KC_W, COMBO_END};
-const uint16_t PROGMEM we_combo[] = {KC_W, KC_E, COMBO_END};
+const uint16_t PROGMEM er_combo[] = {KC_E, KC_R, COMBO_END};
 combo_t key_combos[] = {
     COMBO(qw_combo, KC_ESC),
-    COMBO(we_combo, KC_TAB),
+    COMBO(er_combo, KC_TAB),
 };
