b914249799f1065cf1c10c518c90edab0e1cf3e2
Verf <verf@proton.me>
2026-06-13 08:57:57 +0800

    add lgui

--- keyboards/cantor/keymaps/verf/keymap.c
+++ keyboards/cantor/keymaps/verf/keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, KC_NO, KC_PGUP, KC_UP,   KC_PGDN, KC_HOME, /* */ KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_NO, KC_DEL,
         KC_NO,   KC_NO, KC_LEFT, KC_DOWN, KC_RGHT, KC_END,  /* */ KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_NO, KC_NO,
         KC_TRNS, KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,   /* */ KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,
-                                 KC_TRNS, KC_NO,   QK_BOOT, /* */ KC_NO,   KC_NO,   LCA(KC_DEL)
+                                 KC_TRNS, KC_NO,   QK_BOOT, /* */ KC_LGUI,   KC_NO,   LCA(KC_DEL)
     ),
 };
 
