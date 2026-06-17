f270683e86c1ba2e3f7c5de5cfc219a379a4989d
Verf <verf@proton.me>
2026-06-17 17:59:02 +0800

    switch 1 and 3 layer

--- keyboards/cantor/keymaps/verf/keymap.c
+++ keyboards/cantor/keymaps/verf/keymap.c
@@ -12,7 +12,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,  KC_Q, KC_W,         KC_E,         KC_R,         KC_T,   /* */ KC_Y,    KC_U,         KC_I,         KC_O,         KC_P,    KC_BSLS,
         CW_TOGG, KC_A, LCTL_T(KC_S), LSFT_T(KC_D), LALT_T(KC_F), KC_G,   /* */ KC_H,    LALT_T(KC_J), RSFT_T(KC_K), RCTL_T(KC_L), KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z, KC_X,         KC_C,         KC_V,         KC_B,   /* */ KC_N,    KC_M,         KC_COMM,      KC_DOT,       KC_SLSH, KC_GRV,
-                                     MO(3),        LT(1,KC_SPC), KC_TAB, /* */ KC_LGUI, LT(2,KC_ENT), KC_BSPC
+                                     MO(1),        LT(3,KC_SPC), KC_TAB, /* */ KC_LGUI, LT(2,KC_ENT), KC_BSPC
     ),
     /*
      * SYM
