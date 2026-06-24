b8e5041ae48e25cde4895dd747090012c00e944b
Verf <verf@proton.me>
2026-06-24 11:41:11 +0800

    add lgui

--- keyboards/cantor/keymaps/verf/keymap.c
+++ keyboards/cantor/keymaps/verf/keymap.c
@@ -19,16 +19,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
     [1] = LAYOUT_split_3x6_3(
         KC_TRNS, KC_1,    KC_2,    KC_3,    KC_4,    KC_AMPR, /* */ KC_PLUS, KC_UNDS, KC_LBRC, KC_RBRC, KC_COLN, KC_PIPE,
-        KC_TRNS, OS_LGUI, OS_LCTL, OS_LSFT, OS_LALT, KC_PERC, /* */ KC_MINS, KC_EQL,  KC_LPRN, KC_RPRN, KC_AMPR, KC_DQUO,
+        KC_LGUI, OS_LGUI, OS_LCTL, OS_LSFT, OS_LALT, KC_PERC, /* */ KC_MINS, KC_EQL,  KC_LPRN, KC_RPRN, KC_AMPR, KC_DQUO,
         KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_ASTR, /* */ KC_CIRC, KC_DLR,  KC_LCBR, KC_RCBR, KC_QUES, KC_TILD,
-                                   KC_TRNS, KC_TRNS, QK_BOOT, /* */ KC_ENT,  KC_TRNS, MO(3)
+                                   KC_TRNS, KC_TRNS, QK_BOOT, /* */ KC_TRNS, KC_TRNS, MO(3)
     ),
     /*
      * NUM
      */
     [2] = LAYOUT_split_3x6_3(
         KC_F1, KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   /* */ KC_PEQL, KC_P7,   KC_P8, KC_P9, KC_PMNS, KC_DEL,
-        KC_NO, OS_LGUI, OS_LCTL, OS_LSFT, OS_LALT, KC_NO,   /* */ KC_PDOT, KC_P4,   KC_P5, KC_P6, KC_PPLS, KC_NO,
+        KC_LGUI, OS_LGUI, OS_LCTL, OS_LSFT, OS_LALT, KC_NO,   /* */ KC_PDOT, KC_P4,   KC_P5, KC_P6, KC_PPLS, KC_NO,
         KC_F7, KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  /* */ KC_P0,   KC_P1,   KC_P2, KC_P3, KC_PSLS, KC_NO,
                                  MO(3),   KC_TRNS, KC_TRNS, /* */ KC_TRNS, KC_TRNS, KC_TRNS
     ),
