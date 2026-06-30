dd9dac79fd3b54de7d80910c46b081b157a0f451
Verf <verf@proton.me>
2026-06-30 08:32:31 +0800

    adjust symbol

--- keyboards/cantor/keymaps/verf/keymap.c
+++ keyboards/cantor/keymaps/verf/keymap.c
@@ -50,9 +50,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * BASE (Gallium)
      */
     [1] = LAYOUT_split_3x6_3(
-        KC_ESC,  KC_B, KC_L, KC_D,    KC_C,         KC_V,   /* */  KC_J,    KC_Y,         KC_O,    KC_U,    KC_COMM, KC_BSLS,
-        CW_TOGG, KC_N, KC_R, KC_T,    KC_S,         KC_G,   /* */  KC_P,    KC_H,         KC_A,    KC_E,    KC_I,    KC_SLSH,
-        KC_LSFT, KC_X, KC_Q, KC_M,    KC_W,         KC_Z,   /* */  KC_K,    KC_F,         KC_QUOT, KC_SCLN, KC_DOT,  KC_GRV,
+        KC_ESC,  KC_B, KC_L, KC_D,    KC_C,         KC_V,   /* */  KC_J,    KC_Y,         KC_O,    KC_U,    KC_SCLN, KC_BSLS,
+        CW_TOGG, KC_N, KC_R, KC_T,    KC_S,         KC_G,   /* */  KC_P,    KC_H,         KC_A,    KC_E,    KC_I,    KC_QUOT,
+        KC_LSFT, KC_X, KC_Q, KC_M,    KC_W,         KC_Z,   /* */  KC_K,    KC_F,         KC_COMM, KC_DOT,  KC_SLSH, KC_GRV,
                              KC_TAB,  LT(3,KC_SPC), KC_LGUI, /* */ KC_LSFT, LT(4,KC_ENT), KC_BSPC
     ),
     /*
