a6aa1703ace435f9b73e5cf2d48dd05288c503f8
Verf <verf@proton.me>
2026-07-09 16:13:49 +0800

    fix ESC for cmodv

--- keyboards/cantor/keymaps/verf/keymap.c
+++ keyboards/cantor/keymaps/verf/keymap.c
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * BASE (Colemak-Mod-V)
      */
     [1] = LAYOUT_split_3x6_3(
-        KC_ESC,      KC_Q, KC_W, KC_D,  KC_F,   KC_P,    /* */ KC_Y,    KC_U,   KC_J,    KC_K,   KC_SCLN, KC_BSLS,
+        KC_TAB,      KC_Q, KC_W, KC_D,  KC_F,   KC_P,    /* */ KC_Y,    KC_U,   KC_J,    KC_K,   KC_SCLN, KC_BSLS,
         TD(TD_CWTG), KC_A, KC_S, KC_R,  KC_T,   KC_G,    /* */ KC_H,    KC_N,   KC_E,    KC_O,   KC_I,    KC_QUOT,
         KC_LSFT,     KC_Z, KC_X, KC_C,  KC_V,   KC_B,    /* */ KC_L,    KC_M,   KC_COMM, KC_DOT, KC_SLSH, KC_GRV,
                                  MO(3), KC_SPC, KC_LALT, /* */ KC_LGUI, KC_ENT, MO(4)
